9dbcf3d14a537661f839a710afae7e1d35721566
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-05-11 23:45:25 +0700

    Update belajar-say-hello dependency to version 0.2.0
    The belajar-say-hello dependency was updated from version 0.1.0 to version 0.2.0 in the Cargo.toml and Cargo.lock files. Also, new functions, say_hello_to_everyone and say_goodbye_to_everyone, from the updated dependency were imported and called in the main.rs file.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use belajar_say_hello::{say_hello, say_goodbye};
+use belajar_say_hello::{say_hello, say_goodbye, say_hello_to_everyone, say_goodbye_to_everyone};
 
 fn main() {
     let response = say_hello("Eko");
     println!("{}", response);
 
+    let response = say_hello_to_everyone();
+    println!("{}", response);
+
     let response = say_goodbye("Eko");
+    println!("{}", response);
+
+    let response = say_goodbye_to_everyone();
     println!("{}", response);
 }
